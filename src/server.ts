03890d9eda00722c811558cc0287cc6c2ac9b0ed
FelipeGoncalvesAlmeida <160501854+FelipeGoncalvesAlmeida@users.noreply.github.com>
2024-05-07 18:52:01 +0000

    Aula1

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) =>{
+res.send("Ola Mundo ...")
+})
+
+app.get("/testa", (req, res) =>{
+    res.send(" Sim ")
+    })
+
+app.listen(3000, () => {
+ console.log("Aplicativo esta no ar")
+})
+
+// Para executar, digite node server.js
+//Instalem a extrensão Thunder Client
