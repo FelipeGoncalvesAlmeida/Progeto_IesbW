0f31f04bd79a49a6a72b2efe27429efa75b890bc
FelipeGoncalvesAlmeida <160501854+FelipeGoncalvesAlmeida@users.noreply.github.com>
2024-05-14 19:10:31 +0000

    aluno_onlineW

---
+++ src/Domains/Users /UserRepository.ts
@@ -0,0 +1,23 @@
+import { IUser } from './types'
+
+export class UserRepository {
+  async getUsers() {
+    const db: IUser[] = [
+      {
+        name: "user 1",
+        email: "teste",
+        password: "123"
+      },
+      {
+        name: "user 1",
+        email: "teste2",
+        password: "321"
+      }
+    ]
+
+    return db
+  },
+  async createUser() {
+    const userRepository = AppDataSource.getRepository(User)
+  }
+}
